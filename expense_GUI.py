f691cfb3d142c06a6f36b54d6ec6790e2b3dbbbf
Miki2531 <mikiyasshemelis46@gmail.com>
2024-03-04 23:48:40 +0300

    Expense Tracker

--- expense_GUI.py
+++ expense_GUI.py
@@ -1,5 +1,7 @@
 from tkinter import *
-from expense_tracker import add_expense
+from expense_tracker import add_expense, updateExpense, deleteExpense
+import sqlite3
+import tkinter.simpledialog as sd
 
 
 def add_expense_button_cliked():
@@ -19,46 +21,153 @@ def add_expense_button_cliked():
     description_entry.delete(0, END)
     amount_entry.delete(0, END)
 
+def view_expense():
+    view_window = Toplevel(root)
+    view_window.title("View Expenses")
+    view_window.geometry("400x300")
 
+    conn = sqlite3.connect('expense_tracker.db')
+    cou = conn.cursor() 
+    cou.execute('SELECT * FROM expenses')
+    expenses = cou.fetchall()
+
+    #Add lebel for each fields
+    Label(view_window, text='Date', font=("Arial", 10, "bold")).grid(row=0, column=0)
+    Label(view_window, text='Name', font=("Arial", 10, "bold")).grid(row=0, column=1)
+    Label(view_window, text='Category', font=("Arial", 10, "bold")).grid(row=0, column=2)
+    Label(view_window, text='Description', font=("Arial", 10, "bold")).grid(row=0, column=3)
+    Label(view_window, text='Amount', font=("Arial", 10, "bold")).grid(row=0, column=4)
+
+    for index, expense in enumerate(expenses, start=1):
+        for col, value in enumerate(expense[1:], start=0):
+            Label(view_window, text=value).grid(row=index, column=col)
+        
+    conn.close()
+    
+def update_expense():
+    window_update = Toplevel(root)
+    window_update.title('Update Expense')
+    window_update.geometry("400x300")
+
+    # Connect to the database
+    conn = sqlite3.connect('expense_tracker.db')
+    cou = conn.cursor()
+
+    # Retrieve data for the expense with the given ID
+    id = get_expense_id_somehow()
+    if id != 0:
+        cou.execute('SELECT * FROM expenses WHERE id = ?', (id,))
+        expense_data = cou.fetchone()
+    else: 
+        pass
+
+    Label(window_update, text="Date:").pack()
+    date_entry = Entry(window_update)
+    date_entry.pack()
+    date_entry.insert(0, expense_data[1])
+
+    Label(window_update,  text="Name:").pack()
+    name_entry = Entry(window_update)
+    name_entry.pack()
+    name_entry.insert(0, expense_data[2])
+
+    Label(window_update, text="Category:").pack()
+    category_entry = Entry(window_update)
+    category_entry.pack()
+    category_entry.insert(0, expense_data[3])
+
+    Label(window_update, text="Description:").pack()
+    description_entry = Entry(window_update)
+    description_entry.pack()
+    description_entry.insert(0, expense_data[4])
+
+
+    Label(window_update, text="Amount:").pack()
+    amount_entry = Entry(window_update)
+    amount_entry.pack()
+    amount_entry.insert(0, expense_data[5])
+
+    def update_button_click():
+        update_date = date_entry.get()
+        update_name = name_entry.get()
+        update_category = category_entry.get()
+        update_description = description_entry.get()
+        update_amount = amount_entry.get()
+
+        updateExpense(update_date, update_name, update_category, update_description, update_amount, id)
+        window_update.destroy()
+
+    update_button = Button(window_update, text = "Update Expense", command=update_button_click, font=('Noto Sans CJK TC', 10, 'bold'), bg=hlb_btn_bg)
+ 
+    update_button.pack()
+
+
+
+def get_expense_id_somehow():
+    expense_id = sd.askinteger("Expense ID", "Enter the ID of the expense to update:")
+    return expense_id if expense_id is not None else 0
+
+def update_button_clicked():
+    id = get_expense_id_somehow()  
+    update_expense(id)
+
+def delete_expense():
+    id = get_expense_id_somehow()
+    if id is not None:
+        deleteExpense(id)
+        
 root = Tk()
 root.title("Personal Expense Tracker")
-root.geometry('400x300')
+root.geometry('700x550')
+root.resizable(0, 0)
+
+hlb_btn_bg = 'DarkSalmon'
+btn_font = ('Gill Sans MT', 13)
 
 
-date_label = Label(root, text='Date:')
-date_label.pack()
+date_label = Label(root, text='Date:', font=('Noto Sans CJK TC', 10, 'bold'), bg=hlb_btn_bg)
+date_label.grid(column=0, row=0, padx=20, pady=10)
 
 date_entry = Entry(root)
-date_entry.pack()
+date_entry.grid(column=1, row=0, padx=20, pady=20)
 
-name_label = Label(root, text='Name:')
-name_label.pack()
+name_label = Label(root, text='Name:', font=('Noto Sans CJK TC', 10, 'bold'), bg=hlb_btn_bg)
+name_label.grid(column=0, row=1, padx=20, pady=10)
 
 name_entry = Entry(root)
-name_entry.pack()
+name_entry.grid(column=1, row=1, padx=20, pady=10)
 
 
-category_label = Label(root, text='Category:')
-category_label.pack()
+category_label = Label(root, text='Category:', font=('Noto Sans CJK TC', 10, 'bold'), bg=hlb_btn_bg)
+category_label.grid(column=0, row=2, padx=20, pady=10)
 
 category_entry = Entry(root)
-category_entry.pack()
+category_entry.grid(column=1, row=2, padx=20, pady=10)
 
 
-description_label = Label(root, text='Descriptions:')
-description_label.pack()
+description_label = Label(root, text='Descriptions:', font=('Noto Sans CJK TC', 10, 'bold'), bg=hlb_btn_bg)
+description_label.grid(column=0, row=3, padx=20, pady=10)
 
 description_entry = Entry(root)
-description_entry.pack()
+description_entry.grid(column=1, row=3, padx=20, pady=10)
 
 
-amount_label = Label(root, text='Amount:')
-amount_label.pack()
+amount_label = Label(root, text='Amount:', font=('Noto Sans CJK TC', 10, 'bold'), bg=hlb_btn_bg)
+amount_label.grid(column=0, row=4, padx=20, pady=10)
 
 amount_entry = Entry(root)
-amount_entry.pack()
+amount_entry.grid(column=1, row=4, padx=20, pady=10)
 
-add_button = Button(root, text='Add Expense', command=add_expense_button_cliked)
-add_button.pack()
+add_button = Button(root, text='Add Expense', command=add_expense_button_cliked, font=btn_font, width=11, bg=hlb_btn_bg)
+add_button.grid(column=0, row=5, padx=20, pady=10)
+
+view_button = Button(root, text="View Expenses", command=view_expense, font=btn_font, width=13, bg=hlb_btn_bg)
+view_button.grid(column=1, row=5, padx=20, pady=10)
+
+update_button = Button(root, text="Update Expenses", command=update_expense, font=btn_font, width=15, bg="black", fg="white")
+update_button.grid(column=2, row=5, padx=20, pady=10)
+
+update_button = Button(root, text="Delete Expenses", command=delete_expense, font=btn_font, width=15, bg="black", fg="white")
+update_button.grid(column=3, row=5, padx=20, pady=10)
 
 root.mainloop()
--- expense_tracker.py
+++ expense_tracker.py
@@ -2,6 +2,7 @@ import sqlite3
 from expense import Expense, input_variables
 from datetime import datetime
 
+
 #connect SQLite 3 database
 
 conn = sqlite3.connect('expense_tracker.db')
@@ -23,15 +24,14 @@ CREATE TABLE IF NOT EXISTS expenses (
 conn.commit()
 conn.close()
 
+    
 
 #create the view for create(add)
 def add_expense(date, name, category, description, amount):
 
     expense = Expense(date, name, category, description, amount)
-
-    #connect to the SQLite database
     conn = sqlite3.connect('expense_tracker.db')
-    cou = conn.cursor()
+    cou = conn.cursor()   
 
     #Insert expense into the db
     cou.execute('''
@@ -44,25 +44,29 @@ def add_expense(date, name, category, description, amount):
     conn.close()
 
 
-def view_expense():
+def updateExpense(date, name, category, description, amount, id):
+    expense = Expense(date, name, category, description, amount)
     conn = sqlite3.connect('expense_tracker.db')
-    cou = conn.cursor()
-
-    #Retrive and dispaly all expenses
-    cou.execute('SELECT * FROM expenses')
-    expenses = cou.fetchall()
-
-    print("\nExpense views")
-    for expense in expenses:
-        print(f"ID: {id} Date: {expense[1]},Name: {expense[2]}, Catagory: {expense[3]}, Description: {expense[4]}, Amount : {expense[5]}")
+    cou = conn.cursor()  
+    cou.execute('SELECT id FROM expenses WHERE date = ? AND name = ? AND category = ? AND description = ? AND amount = ?', 
+                (expense.date, expense.name, expense.category, expense.description, expense.amount))
+    
+    conn.commit()
+    print('Expense updated successfully!')
     conn.close()
 
+def deleteExpense(id):
+    conn = sqlite3.connect('expense_tracker.db')
+    cou = conn.cursor() 
+    cou.execute("DELETE FROM expenses WHERE ID = ?", (id,))
+    conn.commit()
+    print("The Expense deleted succesfully!!")
+    conn.close()
 
 
 def calculate_totals():
     conn = sqlite3.connect('expense_tracker.db')
     cou = conn.cursor()
-
     #calculate daily, monthly, and yearly expense total
     cou.execute('SELECT SUM(amount) FROM expense WHERE date = DATE()')
     daily_total = cou.fetchone()[0]
@@ -86,8 +90,8 @@ def main():
     while True:
         print("\nPersonal Expense Tracker")
         print("1. Add Expense")
-        print("2. View Expense")
-        print("3. Calculate Totals")
+        print("2. Calculate Totals")
+        print("3. Delete Expense")
         print("4. Exit")
 
         choice = int(input("Enter your choice: "))
@@ -95,12 +99,13 @@ def main():
             #Adding your expense
             add_expense(input_variables)
             print("Expense Added successfuly!")
-
         elif choice == 2:
-            view_expense()
+            updateExpense(input_variables, id)
         elif choice == 3:
             calculate_totals()
         elif choice == 4:
+            deleteExpense(id)
+        elif choice == 5:
             print("Exiting from Expense Tracker.")
             break
         else:
